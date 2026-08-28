45b4c9fddddd0b50b597e442effa749a91b8d12d
kyleobrien91 <kyleobrien91@users.noreply.github.com>
2026-08-28 12:31:59 +0100

    chore: remove dummy script files left by Jules

--- fix_trace.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs';
-const path = 'src/proxy/stealth/trace.ts';
-let code = fs.readFileSync(path, 'utf8');
-code = code.replace(
-    'export class TraceCollector {',
-    'export class TraceCollector {\n    public startTrace() {}\n    public writeTrace() {}'
-);
-fs.writeFileSync(path, code);
